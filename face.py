f7a98686dedfed8f7016a248a46692add268d574
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-17 08:21:10 -0500

    Updated from Brython Server: 1/17/2018 8:21:09 AM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#1/17/18
+#face.py - prints a creepy lookin face
 
+print('   xxxxx   ')
+print('  x     x  ')
+print('((  o o  ))')
+print('  |  v  |  ')
+print('  | === |  ')
+print('   -----   ')
