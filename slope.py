dab84a6e58c1eded925ae65f49a3a070196d39db
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-18 08:30:12 -0500

    Update slope.py

--- slope.py
+++ slope.py
@@ -8,3 +8,13 @@ x2 = float(input('x2 = '))
 y2 = float(input('y2 = '))
 
 print('The slope is',(y2-y1)/(x2-x1))
+
+
+print('y=mx+b')
+if (x2-x1) == 0:
+    print('Vertical line, not a function')
+elif (y2-y1) == 0:
+    print('Horizontal line at y =',y1)
+else :
+    m = ((y2-y1)/(x2-x1))
+    print('m = ',m,'and b = ',(y1-(x1*m)))
