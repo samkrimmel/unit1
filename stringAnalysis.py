1ae7d51cae8c89cd301dbe83b5a840c295b84ab0
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-18 08:27:23 -0500

    Updated from Brython Server: 1/18/2018 8:27:21 AM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,17 @@
+#Sam Krimmel
+#1/18/18
+#stringAnalysis.py - reports data about a given sentence
+
+sent = input('Enter a sentence: ')
+spac = sent.count(' ')
+char = len(sent)
+print('Your sentence has', spac+1, 'words and', len(sent), 'characters and', char-spac, 'letters.')
+
+schar = input('Enter a character to search for: ')
+csent = sent.upper()
+print('Your sentence has', csent.count(schar.upper()),'of the letter', schar)
+
+sword = input('Enter a word to search for: ')
+print('Your sentence has', sent.count(sword), 'of the word', sword)
+
 
