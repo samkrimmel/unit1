ae1612fa5a588d08ff5eabf58dbc66a0e8ff13ca
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-18 08:29:02 -0500

    Update slope.py

--- slope.py
+++ slope.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#1/16/18
+#slope.py - calculates the slope between two points
 
+x1 = float(input('x1 = '))
+y1 = float(input('y1 = '))
+x2 = float(input('x2 = '))
+y2 = float(input('y2 = '))
+
+print('The slope is',(y2-y1)/(x2-x1))
