3a7cdd263ab304661dc80d44d3e0c09d49ae2a92
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-19 08:43:26 -0500

    Updated from Brython Server: 1/19/2018 8:43:24 AM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,8 @@
+#Sam Krimmel
+#1/19/18
+#eggs.py - asks user for # of eggs and # of egg cartons, prints true if eggs fit cartons
 
+e = int(input('Input number of eggs: '))
+c = int(input('Input the number of Egg Cartons: '))
+
+print((c*12)>=e)
