73e8bd16f48babca24447d521bc53b6b827f8268
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-22 08:48:06 -0500

    Updated from Brython Server: 1/22/2018 8:48:04 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,12 @@
+#Sam Krimmel
+#1/22/18
+#isItATriangle.py - tells whether three sides can make a triangle or not
+
+a = int(input('Enter side A: '))
+b = int(input('Enter side B: '))
+c = int(input('Enter side C: '))
+
+s = min(a,b,c)
+
+print(A + B + C == 180)
 
