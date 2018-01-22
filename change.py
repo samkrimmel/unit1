955b91794fcf743061e85ff2fc9175813892d4c7
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-22 08:39:20 -0500

    Updated from Brython Server: 1/22/2018 8:39:19 AM

--- change.py
+++ change.py
@@ -1,1 +1,15 @@
+#Sam Krimmel
+#1/22/18
+#change.py - takes in amount of money and prints how many quarters, dimes, nickels, and pennies you need to make up that amount
 
+cash = int(input('Amount of money in cents: '))
+
+quar = cash//25
+dime = (cash - 25*quar)//10
+nick = (cash - (dime*10 + quar*25))//5
+penn = cash - (dime*10 + quar*25 + nick*5)
+
+print('Quarters: ', quar)
+print('Dimes: ', dime)
+print('Nickels: ', nick)
+print('Pennies: ', penn)
