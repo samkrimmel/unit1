147505cc470614ae155211e37aff902585f3c630
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-22 08:11:41 -0500

    Updated from Brython Server: 1/22/2018 8:11:39 AM

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -1,1 +1,14 @@
+#Sam Krimmel
+#1/22/18
+#additionGameDemo.py - how to use random numbers
 
+from random import randint
+
+num1 = randint(-10,10)
+num2 = randint(-10,10)
+
+print(num1, '+', num2)
+
+ans = int(input('='))
+
+print(ans == num1 + num2)
