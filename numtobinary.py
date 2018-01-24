5210d89456eff5cd87f898ea52a07fe7a69e5b30
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-24 08:25:10 -0500

    Updated from Brython Server: 1/24/2018 8:25:06 AM

--- numtobinary.py
+++ numtobinary.py
@@ -2,7 +2,7 @@
 #1/24/18
 #numtobinary.py - converts a number to eight digit binary
 
-num = int(input('Enter a number less than 255: '))
+num = int(input('Enter a number to convert to binary: '))
 
 binary = bin(num)
 
