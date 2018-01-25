86ff7d2d0a37cff40df4f07d376b43e3193150a7
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-25 08:17:07 -0500

    Updated from Brython Server: 1/25/2018 8:17:06 AM

--- warmup2.py
+++ warmup2.py
@@ -1,1 +1,9 @@
+#Sam Krimmel
+#1/25/18
+#warmup2.py - gives someone theur lucky number of the day
 
+from random import randint
+
+name = (input('What is your name?'))
+
+print('Hi',name,'your lucky number is', randint(1,100))
