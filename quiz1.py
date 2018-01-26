db6575a07777b8c6616d4869562644578215b29c
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-26 08:14:03 -0500

    Updated from Brython Server: 1/26/2018 8:14:02 AM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#1/26/18
+#quiz1.py - quiz program for unit one
 
+print('Hello')
+name = input('Enter your name: ')
+num1 = int(input('Enter your favorite number: '))
+print(num1*name)
+num2 = int(input('Enter your second favorite number: '))
+print('Your lucky number is', num1**num2)
